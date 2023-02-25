36df1f22d1f24cc4bfeee17092025a603f3d016f
ZaharKondaurov <stallmens@yadnex.ru>
2023-02-25 19:45:42 +0300

    Add recognition of selected words(click only)

--- extension/hover/hover.js
+++ extension/hover/hover.js
@@ -1,3 +1,29 @@
+function getWords() {
+    var selected = window.getSelection()
+    if(selected.rangeCount > 0) {
+        var range = selected.getRangeAt(0).cloneRange()
+
+        if (range.startOffset === range.endOffset) {
+            let startWord;
+            let endWord;
+            startWord = (r) => r.toString().match(/^\s/)
+            endWord = (r) => r.toString().match(/\s$/)
+
+            while (!startWord(range) && range.startOffset > 0) {
+                range.setStart(range.startContainer, range.startOffset - 1)
+            }
+            if (startWord(range)) range.setStart(range.startContainer, range.startOffset + 1)
+
+            var length = range.endContainer.length || range.endContainer.childNodes.length
+            while (!endWord(range) && range.endOffset < length) {
+                range.setEnd(range.endContainer, range.endOffset + 1)
+            }
+            if (endWord(range) && range.endOffset > 0) range.setEnd(range.endContainer, range.endOffset - 1)
+
+        }
+    }
+    return range.toString()
+}
 
 function getText(el) {
     let ret = "";
@@ -16,10 +42,10 @@ function getCount(el) {
     return words.split(' ').length;
 }
 
-makeHover = function(e) {
+var makeHover = function(e) {
     var target = e.target;
 
-    if (prevDom !== target && target.textContent) {
+    if (target.textContent) {
 
         if (prevDom != null) {
             prevDom.classList.remove(MVC);
@@ -28,7 +54,7 @@ makeHover = function(e) {
 
         const span = document.createElement('span');
         span.classList.add('tooltiptext');
-        span.appendChild(document.createTextNode("Contains text! Word count: " + getCount(target)));
+        span.appendChild(document.createTextNode("Word was founded: " + getWords()));
 
         target.classList.add(MVC);
         target.appendChild(span)
@@ -44,7 +70,7 @@ var prevDom = null;
 var prevSpan = null;
 
 setInterval(() => {
-    document.addEventListener('mousemove', makeHover, {once: true})
-}, 350)
+    document.addEventListener('click', makeHover, {once: true})
+}, 0)
 
 
