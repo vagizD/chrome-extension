194de0f4018b497b9610b777244564b20325c22b
vagiz.daudov <vagiz.0282@gmail.com>
2023-06-08 13:51:20 +0300

    logs change

--- server/logs/logs.py
+++ server/logs/logs.py
@@ -1,8 +1,10 @@
 import logging
-import os
-from definitions import ROOT_DIR
+from os.path import dirname, join, abspath
+
+parent_dir = dirname(abspath(__file__))
+logs_file = join(parent_dir, 'server_logs.txt')
 
 logging_config = {"format": '%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                   "level": logging.INFO,  # make "level": logging.DEBUG for dev purposes
-                  "filename": os.path.join(ROOT_DIR, 'server', 'logs', 'server_logs.txt'),
+                  "filename": logs_file,
                   "filemode": "a+"}
